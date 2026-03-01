75e69d2f067da80b2ea16573ee195700ec5daab2
jacques <j.dogbevi@kommit5.de>
2026-03-01 11:58:13 +0000

    Update ESLint configuration to include parser options and additional TypeScript rules

--- eslint.config.js
+++ eslint.config.js
@@ -18,6 +18,15 @@ export default defineConfig([
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+    rules: {
+      '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_', varsIgnorePattern: '^_' }],
+      '@typescript-eslint/require-await': 'error',
+      '@typescript-eslint/no-floating-promises': 'error',
     },
   },
 ])
